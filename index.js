5fbbcfedd2a6870ccd5ad81b794293ce426eaa2f
ted Liefeld <liefeld@broadinstitute.org>
2017-07-24 14:08:23 +0200

    switch port to accept it as an env variable from heroku

--- index.js
+++ index.js
@@ -48,7 +48,8 @@ io.on('connection', function(socket){
 
 });
 
-http.listen(3000, function(){
-     console.log('listening on *:3000');
+var port = process.env.PORT || 3000;
+http.listen(port, function(){
+     console.log('listening on :' + port);
 });
 
